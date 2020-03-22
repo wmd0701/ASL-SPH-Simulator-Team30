084173e36aafedd3bbfd6646459ee3ea27244ef7
Tianwei Yu <tianyu@student.ethz.ch>
2020-03-22 13:17:40 +0100

    Upload New File

---
+++ tianwei/data_set.h
@@ -0,0 +1,48 @@
+
+//! @file data_set.h
+
+typedef int index;
+
+typedef std::pair<double,double> vector;
+
+
+/**  
+*	 @brief A struct containing some variables of a particle
+*/
+typedef struct {
+	
+	vector   position;      //!< 2d coordinate
+	double   mass;          //!< mass
+	vector   velocity;      //!< 2d velocity
+	double   density;       //!< the value of density field 
+	double   pressure;      //!< the value of pressure field
+	double 	 accelerat;     //!< acceleration of the particle, namely dv/dt
+	
+	index    tag;           //!< whether it's an interior particle, repulsive particle or ghost particle
+		
+	index*   ptc_nearby;    //!< array containing the index array of nearby particles 
+	double*  Wij;           /*!< array containing the value of kernel function w.r.t. the nearby particles. 
+						         The sequence is corresponding to the sequence in ptc_nearby */
+	vector*  Wij_grad;      /*!< 2 x N matrix, N is the total number of nearby particles.  
+								 The sequence is corresponding to the sequence in ptc_nearby */
+	double   L              //!< used for kernel gradient correction : ▽W_new = L * ▽W
+	
+								
+} Particle;
+
+
+/**  
+*    @brief search for the nearby particles 
+*    @param all_particle pointer to an array containing information of all the particles
+*    @param par_idx index of the particle that is being considered
+*    @param h searching radius (smoothing length)
+*    @return pointer to an array containing the index of nearby particles
+*/
+index* SearchNearby (Particle*& all_particle, int par_idx, double h) {
+	
+}
+
+
+	
+
+
