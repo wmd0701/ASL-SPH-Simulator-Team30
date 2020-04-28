ab66b348f34d282f797a824fbf88e5805ab6b2e1
Mengdi Wang <wangmengdi0701@hotmail.com>
2020-04-28 11:00:51 +0200

    comments in bash code
    Former-commit-id: 8168ff7b4097e38ff8b27982d9c97001a855395f

--- simulation_bash.sh
+++ simulation_bash.sh
@@ -1,4 +1,9 @@
 #/bin/bash
+
+# If you modify bash code on Windows, you may get error message "unexpected token \r" when running bash code.
+# The reason lies in character difference between Linux and Windows. To solve the problem, please follow:
+# https://stackoverflow.com/questions/27176781/bash-file-returns-unexpected-token-do-r
+
 for num_particles in 400 800 1200 1600 2000 2400 2800 3200 3600 4000
 do
         mkdir data_num_particles_$num_particles
