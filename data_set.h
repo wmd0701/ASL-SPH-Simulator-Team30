11c12b1de8f4e10d103038cc52aff39c3bee568c
Valerie Kulka <vkulka@student.ethz.ch>
2020-05-11 17:26:50 +0200

    autoformat
    Former-commit-id: 607b6c23908a8dd465612f807ccac1d121eec8fe

--- data_set.h
+++ data_set.h
@@ -92,11 +92,12 @@ typedef struct {
  *      @param flag  1: particle_1 and particle_2 are neighbors of each other
  *                  -1: particle_2 is a neighbor of particle_1 but the opposite is false.
  */
-void KernelAndGradient(Particle* all_particle, vector diff, int par_idx_1, int par_idx_2, double r, int flag) {
+void KernelAndGradient(Particle *all_particle, vector diff, int par_idx_1,
+                       int par_idx_2, double r, int flag) {
   double kernel;
   vector grad;
   double q = r * Hinv;
-	double q2 = q * q;
+  double q2 = q * q;
   double temp, c;
 
   if (q <= 1.) {
@@ -110,7 +111,7 @@ void KernelAndGradient(Particle* all_particle, vector diff, int par_idx_1, int p
       grad.second = 0.;
     }
   } else if (1. <= q && q <= 2.) {
-		c = (2.0 - q) * (2.0 - q);
+    c = (2.0 - q) * (2.0 - q);
     kernel = factor * 0.25 * c * (2.0 - q);
     temp = -factor * 0.75 * c * Hinv / r;
     grad.first = temp * diff.first;
@@ -122,18 +123,21 @@ void KernelAndGradient(Particle* all_particle, vector diff, int par_idx_1, int p
     grad.second = 0;
   }
 
-  all_particle[par_idx_1].Wij     [all_particle[par_idx_1].neighbor_num] = kernel;
+  all_particle[par_idx_1].Wij[all_particle[par_idx_1].neighbor_num] = kernel;
   all_particle[par_idx_1].Wij_grad[all_particle[par_idx_1].neighbor_num] = grad;
-  all_particle[par_idx_1].index   [all_particle[par_idx_1].neighbor_num] = par_idx_2;
+  all_particle[par_idx_1].index[all_particle[par_idx_1].neighbor_num] =
+      par_idx_2;
   all_particle[par_idx_1].neighbor_num += 1;
   if (flag == 1) {
-    
+
     grad.first = -grad.first;
     grad.second = -grad.second;
 
-    all_particle[par_idx_2].Wij     [all_particle[par_idx_2].neighbor_num] = kernel;
-    all_particle[par_idx_2].Wij_grad[all_particle[par_idx_2].neighbor_num] = grad;
-    all_particle[par_idx_2].index   [all_particle[par_idx_2].neighbor_num] = par_idx_1;
+    all_particle[par_idx_2].Wij[all_particle[par_idx_2].neighbor_num] = kernel;
+    all_particle[par_idx_2].Wij_grad[all_particle[par_idx_2].neighbor_num] =
+        grad;
+    all_particle[par_idx_2].index[all_particle[par_idx_2].neighbor_num] =
+        par_idx_1;
     all_particle[par_idx_2].neighbor_num += 1;
   }
 }
@@ -148,37 +152,37 @@ void KernelAndGradient(Particle* all_particle, vector diff, int par_idx_1, int p
 *			 - ghost particles need information of both interior and repulsive particles
 */
 void SearchNeighbors(Particle *all_particle) {
-    vector xi, xj, diff;
-    double r;
+  vector xi, xj, diff;
+  double r;
 
-    vector zero = {0.0, 0.0};
-    for(int i = 0; i < NUMBER_OF_PARTICLE; ++i){
-        all_particle[i].neighbor_num = 0;
-        KernelAndGradient(all_particle, zero, i, i, 0.0, -1);
-    }  
-    
-    for (int i = 0; i < N_interior; i++) {
-      xi = all_particle[i].position;
-      for (int j = i + 1; j < NUMBER_OF_PARTICLE; j++) {
-        xj = all_particle[j].position;
-        r = vec_distance_vec(xi, xj);
-        if (r < Hradius) {
-          diff = vec_sub_vec(xi, xj);
-          KernelAndGradient(all_particle, diff, i, j, r, 1);
-        }
+  vector zero = {0.0, 0.0};
+  for (int i = 0; i < NUMBER_OF_PARTICLE; ++i) {
+    all_particle[i].neighbor_num = 0;
+    KernelAndGradient(all_particle, zero, i, i, 0.0, -1);
+  }
+
+  for (int i = 0; i < N_interior; i++) {
+    xi = all_particle[i].position;
+    for (int j = i + 1; j < NUMBER_OF_PARTICLE; j++) {
+      xj = all_particle[j].position;
+      r = vec_distance_vec(xi, xj);
+      if (r < Hradius) {
+        diff = vec_sub_vec(xi, xj);
+        KernelAndGradient(all_particle, diff, i, j, r, 1);
       }
     }
-    for (int i = N_interior + N_repulsive; i < NUMBER_OF_PARTICLE; i++) {
-      xi = all_particle[i].position;
-      for (int j = N_interior; j < N_interior + N_repulsive; j++) {
-        xj = all_particle[j].position;
-        r = vec_distance_vec(xi, xj);
-        if (r < Hradius) {
-          diff = vec_sub_vec(xi, xj);
-          KernelAndGradient(all_particle, diff, i, j, r, -1);
-        }
+  }
+  for (int i = N_interior + N_repulsive; i < NUMBER_OF_PARTICLE; i++) {
+    xi = all_particle[i].position;
+    for (int j = N_interior; j < N_interior + N_repulsive; j++) {
+      xj = all_particle[j].position;
+      r = vec_distance_vec(xi, xj);
+      if (r < Hradius) {
+        diff = vec_sub_vec(xi, xj);
+        KernelAndGradient(all_particle, diff, i, j, r, -1);
       }
     }
+  }
 }
 
 
@@ -195,9 +199,10 @@ void SearchNeighbors(Particle *all_particle) {
  */
 Particle *Init() {
   // TODO: initialization
-  Particle *particles = (Particle *)malloc(sizeof(Particle) * NUMBER_OF_PARTICLE);
+  Particle *particles =
+      (Particle *)malloc(sizeof(Particle) * NUMBER_OF_PARTICLE);
   int now = 0;
-  
+
   // Set interior particles
   for (int i = 0; i < Nx_interior; ++i) {
     for (int j = 0; j < Ny_interior; ++j) {
@@ -207,7 +212,7 @@ Particle *Init() {
       ++now;
     }
   }
-	
+
   // Set repulsive particles
   for (int i = 0; i < Nx_boundary; i++) {
     particles[now].position.first = i * H / 2;
@@ -215,7 +220,7 @@ Particle *Init() {
     particles[now].tag = repulsive;
     ++now;
   }
-	
+
   for (int j = 0; j < Ny_boundary; j++) {
     particles[now].position.first = 0;
     particles[now].position.second = j * H / 2;
@@ -228,7 +233,7 @@ Particle *Init() {
     particles[now].tag = repulsive;
     ++now;
   }
-		
+
   // Set ghost particles
   for (int i = -2; i < Nx_boundary + 2; i++) {
     particles[now].position.first = i * H / 2;
@@ -241,7 +246,7 @@ Particle *Init() {
     particles[now].tag = ghost;
     ++now;
   }
-		
+
   for (int j = 0; j < Ny_boundary; j++) {
     particles[now].position.first = -H;
     particles[now].position.second = j * H / 2;
@@ -261,13 +266,13 @@ Particle *Init() {
     particles[now].position.first = (Nx_interior + 2) * H;
     particles[now].position.second = j * H / 2;
     particles[now].tag = ghost;
-		++now;
-	}
-	
-  //printf("%i \n", now);
+    ++now;
+  }
+
+  // printf("%i \n", now);
   if (NUMBER_OF_PARTICLE != now)
-    printf("number of particles doesn't match with init,\n");  
-	
+    printf("number of particles doesn't match with init,\n");
+
   int N = NUMBER_OF_PARTICLE;   // get the number of particles
   for (int i = 0; i < N; i++) { // traverse particles
     particles[i].position.first += amplitude;
