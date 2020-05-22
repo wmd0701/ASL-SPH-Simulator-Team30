97f0a53a645de039b518fef7c202df2bb8440367
Mengdi Wang <wangmengdi0701@hotmail.com>
2020-05-22 10:09:08 +0200

    unrolling factor from 16 to 4
    Former-commit-id: 42a05c9f3f14ba13e5e3283afd6d843bb8b1db4a

--- data_set.h
+++ data_set.h
@@ -131,17 +131,15 @@ void ClearNeighbors(){
 
 void SearchNeighbors() {
   int block_size = 64;          // size of block
-  int unrolling_factor1 = 16;   // unrolling factor 1
+  int unrolling_factor1 = 4;    // unrolling factor 1
   int unrolling_factor2 = 4;    // unrolling factor 2
   int block_end_i, block_end_j; // end of array that fits into block, e.g. if array size is 230, block size is 50, then block end is 200, 
                                 // the array can be divided into 4 block, with 30 elements left that do not fit into a block
   int block_i, block_j;         // begin of block
   int limit_i, limit_j;         //  end  of block
   
-  vector xi, xj1, xj2, xj3, xj4, xj5, xj6, xj7, xj8, xj9, xj10, xj11, xj12, xj13, xj14, xj15, xj16,
-         diff1, diff2, diff3, diff4, diff5, diff6, diff7, diff8, diff9, diff10, diff11, diff12, 
-         diff13, diff14, diff15, diff16;
-  double r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16;
+  vector xi, xj1, xj2, xj3, xj4, diff1, diff2, diff3, diff4;
+  double r1, r2, r3, r4;
   int unrolling_limit;          // unrolling limit
   int i, j;
   
@@ -209,82 +207,22 @@ void SearchNeighbors() {
           xj2  = positions[j + 1];
           xj3  = positions[j + 2];
           xj4  = positions[j + 3];
-          xj5  = positions[j + 4];
-          xj6  = positions[j + 5];
-          xj7  = positions[j + 6];
-          xj8  = positions[j + 7];
-          xj9  = positions[j + 8];
-          xj10 = positions[j + 9];
-          xj11 = positions[j + 10];
-          xj12 = positions[j + 11];
-          xj13 = positions[j + 12];
-          xj14 = positions[j + 13];
-          xj15 = positions[j + 14];
-          xj16 = positions[j + 15];
           diff1  = (vector){xi.first - xj1.first, xi.second - xj1.second};
           diff2  = (vector){xi.first - xj2.first, xi.second - xj2.second};
           diff3  = (vector){xi.first - xj3.first, xi.second - xj3.second};
           diff4  = (vector){xi.first - xj4.first, xi.second - xj4.second};
-          diff5  = (vector){xi.first - xj5.first, xi.second - xj5.second};
-          diff6  = (vector){xi.first - xj6.first, xi.second - xj6.second};
-          diff7  = (vector){xi.first - xj7.first, xi.second - xj7.second};
-          diff8  = (vector){xi.first - xj8.first, xi.second - xj8.second};
-          diff9  = (vector){xi.first - xj9.first, xi.second - xj9.second};
-          diff10 = (vector){xi.first - xj10.first, xi.second - xj10.second};
-          diff11 = (vector){xi.first - xj11.first, xi.second - xj11.second};
-          diff12 = (vector){xi.first - xj12.first, xi.second - xj12.second};
-          diff13 = (vector){xi.first - xj13.first, xi.second - xj13.second};
-          diff14 = (vector){xi.first - xj14.first, xi.second - xj14.second};
-          diff15 = (vector){xi.first - xj15.first, xi.second - xj15.second};
-          diff16 = (vector){xi.first - xj16.first, xi.second - xj16.second};
           r1  = diff1.first  * diff1.first  + diff1.second  * diff1.second;
           r2  = diff2.first  * diff2.first  + diff2.second  * diff2.second;
           r3  = diff3.first  * diff3.first  + diff3.second  * diff3.second;
           r4  = diff4.first  * diff4.first  + diff4.second  * diff4.second;
-          r5  = diff5.first  * diff5.first  + diff5.second  * diff5.second;
-          r6  = diff6.first  * diff6.first  + diff6.second  * diff6.second;
-          r7  = diff7.first  * diff7.first  + diff7.second  * diff7.second;
-          r8  = diff8.first  * diff8.first  + diff8.second  * diff8.second;
-          r9  = diff9.first  * diff9.first  + diff9.second  * diff9.second;
-          r10 = diff10.first * diff10.first + diff10.second * diff10.second;
-          r11 = diff11.first * diff11.first + diff11.second * diff11.second;
-          r12 = diff12.first * diff12.first + diff12.second * diff12.second;
-          r13 = diff13.first * diff13.first + diff13.second * diff13.second;
-          r14 = diff14.first * diff14.first + diff14.second * diff14.second;
-          r15 = diff15.first * diff15.first + diff15.second * diff15.second;
-          r16 = diff16.first * diff16.first + diff16.second * diff16.second;
           r1  = sqrt(r1);
           r2  = sqrt(r2);
           r3  = sqrt(r3);
           r4  = sqrt(r4);
-          r5  = sqrt(r5);
-          r6  = sqrt(r6);
-          r7  = sqrt(r7);
-          r8  = sqrt(r8);
-          r9  = sqrt(r9);
-          r10 = sqrt(r10);
-          r11 = sqrt(r11);
-          r12 = sqrt(r12);
-          r13 = sqrt(r13);
-          r14 = sqrt(r14);
-          r15 = sqrt(r15);
-          r16 = sqrt(r16);
           if(r1  < Hradius) KernelAndGradient_bidirectional(diff1, i, j    , r1);
           if(r2  < Hradius) KernelAndGradient_bidirectional(diff2, i, j + 1, r2);
           if(r3  < Hradius) KernelAndGradient_bidirectional(diff3, i, j + 2, r3);
           if(r4  < Hradius) KernelAndGradient_bidirectional(diff4, i, j + 3, r4);
-          if(r5  < Hradius) KernelAndGradient_bidirectional(diff5, i, j + 4, r5);
-          if(r6  < Hradius) KernelAndGradient_bidirectional(diff6, i, j + 5, r6);
-          if(r7  < Hradius) KernelAndGradient_bidirectional(diff7, i, j + 6, r7);
-          if(r8  < Hradius) KernelAndGradient_bidirectional(diff8, i, j + 7, r8);
-          if(r9  < Hradius) KernelAndGradient_bidirectional(diff9, i, j + 8, r9);
-          if(r10 < Hradius) KernelAndGradient_bidirectional(diff10, i, j + 9, r10);
-          if(r11 < Hradius) KernelAndGradient_bidirectional(diff11, i, j + 10, r11);
-          if(r12 < Hradius) KernelAndGradient_bidirectional(diff12, i, j + 11, r12);
-          if(r13 < Hradius) KernelAndGradient_bidirectional(diff13, i, j + 12, r13);
-          if(r14 < Hradius) KernelAndGradient_bidirectional(diff14, i, j + 13, r14);
-          if(r15 < Hradius) KernelAndGradient_bidirectional(diff15, i, j + 14, r15);
-          if(r16 < Hradius) KernelAndGradient_bidirectional(diff16, i, j + 15, r16);
         }
 
       }
@@ -396,82 +334,22 @@ void SearchNeighbors() {
           xj2  = positions[j + 1];
           xj3  = positions[j + 2];
           xj4  = positions[j + 3];
-          xj5  = positions[j + 4];
-          xj6  = positions[j + 5];
-          xj7  = positions[j + 6];
-          xj8  = positions[j + 7];
-          xj9  = positions[j + 8];
-          xj10 = positions[j + 9];
-          xj11 = positions[j + 10];
-          xj12 = positions[j + 11];
-          xj13 = positions[j + 12];
-          xj14 = positions[j + 13];
-          xj15 = positions[j + 14];
-          xj16 = positions[j + 15];
           diff1  = (vector){xi.first - xj1.first, xi.second - xj1.second};
           diff2  = (vector){xi.first - xj2.first, xi.second - xj2.second};
           diff3  = (vector){xi.first - xj3.first, xi.second - xj3.second};
           diff4  = (vector){xi.first - xj4.first, xi.second - xj4.second};
-          diff5  = (vector){xi.first - xj5.first, xi.second - xj5.second};
-          diff6  = (vector){xi.first - xj6.first, xi.second - xj6.second};
-          diff7  = (vector){xi.first - xj7.first, xi.second - xj7.second};
-          diff8  = (vector){xi.first - xj8.first, xi.second - xj8.second};
-          diff9  = (vector){xi.first - xj9.first, xi.second - xj9.second};
-          diff10 = (vector){xi.first - xj10.first, xi.second - xj10.second};
-          diff11 = (vector){xi.first - xj11.first, xi.second - xj11.second};
-          diff12 = (vector){xi.first - xj12.first, xi.second - xj12.second};
-          diff13 = (vector){xi.first - xj13.first, xi.second - xj13.second};
-          diff14 = (vector){xi.first - xj14.first, xi.second - xj14.second};
-          diff15 = (vector){xi.first - xj15.first, xi.second - xj15.second};
-          diff16 = (vector){xi.first - xj16.first, xi.second - xj16.second};
           r1  = diff1.first  * diff1.first  + diff1.second  * diff1.second;
           r2  = diff2.first  * diff2.first  + diff2.second  * diff2.second;
           r3  = diff3.first  * diff3.first  + diff3.second  * diff3.second;
           r4  = diff4.first  * diff4.first  + diff4.second  * diff4.second;
-          r5  = diff5.first  * diff5.first  + diff5.second  * diff5.second;
-          r6  = diff6.first  * diff6.first  + diff6.second  * diff6.second;
-          r7  = diff7.first  * diff7.first  + diff7.second  * diff7.second;
-          r8  = diff8.first  * diff8.first  + diff8.second  * diff8.second;
-          r9  = diff9.first  * diff9.first  + diff9.second  * diff9.second;
-          r10 = diff10.first * diff10.first + diff10.second * diff10.second;
-          r11 = diff11.first * diff11.first + diff11.second * diff11.second;
-          r12 = diff12.first * diff12.first + diff12.second * diff12.second;
-          r13 = diff13.first * diff13.first + diff13.second * diff13.second;
-          r14 = diff14.first * diff14.first + diff14.second * diff14.second;
-          r15 = diff15.first * diff15.first + diff15.second * diff15.second;
-          r16 = diff16.first * diff16.first + diff16.second * diff16.second;
           r1  = sqrt(r1);
           r2  = sqrt(r2);
           r3  = sqrt(r3);
           r4  = sqrt(r4);
-          r5  = sqrt(r5);
-          r6  = sqrt(r6);
-          r7  = sqrt(r7);
-          r8  = sqrt(r8);
-          r9  = sqrt(r9);
-          r10 = sqrt(r10);
-          r11 = sqrt(r11);
-          r12 = sqrt(r12);
-          r13 = sqrt(r13);
-          r14 = sqrt(r14);
-          r15 = sqrt(r15);
-          r16 = sqrt(r16);
           if(r1  < Hradius) KernelAndGradient_bidirectional(diff1, i, j    , r1);
           if(r2  < Hradius) KernelAndGradient_bidirectional(diff2, i, j + 1, r2);
           if(r3  < Hradius) KernelAndGradient_bidirectional(diff3, i, j + 2, r3);
           if(r4  < Hradius) KernelAndGradient_bidirectional(diff4, i, j + 3, r4);
-          if(r5  < Hradius) KernelAndGradient_bidirectional(diff5, i, j + 4, r5);
-          if(r6  < Hradius) KernelAndGradient_bidirectional(diff6, i, j + 5, r6);
-          if(r7  < Hradius) KernelAndGradient_bidirectional(diff7, i, j + 6, r7);
-          if(r8  < Hradius) KernelAndGradient_bidirectional(diff8, i, j + 7, r8);
-          if(r9  < Hradius) KernelAndGradient_bidirectional(diff9, i, j + 8, r9);
-          if(r10 < Hradius) KernelAndGradient_bidirectional(diff10, i, j + 9, r10);
-          if(r11 < Hradius) KernelAndGradient_bidirectional(diff11, i, j + 10, r11);
-          if(r12 < Hradius) KernelAndGradient_bidirectional(diff12, i, j + 11, r12);
-          if(r13 < Hradius) KernelAndGradient_bidirectional(diff13, i, j + 12, r13);
-          if(r14 < Hradius) KernelAndGradient_bidirectional(diff14, i, j + 13, r14);
-          if(r15 < Hradius) KernelAndGradient_bidirectional(diff15, i, j + 14, r15);
-          if(r16 < Hradius) KernelAndGradient_bidirectional(diff16, i, j + 15, r16);
         }
 
       }
@@ -578,82 +456,22 @@ void SearchNeighbors() {
           xj2  = positions[j + 1];
           xj3  = positions[j + 2];
           xj4  = positions[j + 3];
-          xj5  = positions[j + 4];
-          xj6  = positions[j + 5];
-          xj7  = positions[j + 6];
-          xj8  = positions[j + 7];
-          xj9  = positions[j + 8];
-          xj10 = positions[j + 9];
-          xj11 = positions[j + 10];
-          xj12 = positions[j + 11];
-          xj13 = positions[j + 12];
-          xj14 = positions[j + 13];
-          xj15 = positions[j + 14];
-          xj16 = positions[j + 15];
           diff1  = (vector){xi.first - xj1.first, xi.second - xj1.second};
           diff2  = (vector){xi.first - xj2.first, xi.second - xj2.second};
           diff3  = (vector){xi.first - xj3.first, xi.second - xj3.second};
           diff4  = (vector){xi.first - xj4.first, xi.second - xj4.second};
-          diff5  = (vector){xi.first - xj5.first, xi.second - xj5.second};
-          diff6  = (vector){xi.first - xj6.first, xi.second - xj6.second};
-          diff7  = (vector){xi.first - xj7.first, xi.second - xj7.second};
-          diff8  = (vector){xi.first - xj8.first, xi.second - xj8.second};
-          diff9  = (vector){xi.first - xj9.first, xi.second - xj9.second};
-          diff10 = (vector){xi.first - xj10.first, xi.second - xj10.second};
-          diff11 = (vector){xi.first - xj11.first, xi.second - xj11.second};
-          diff12 = (vector){xi.first - xj12.first, xi.second - xj12.second};
-          diff13 = (vector){xi.first - xj13.first, xi.second - xj13.second};
-          diff14 = (vector){xi.first - xj14.first, xi.second - xj14.second};
-          diff15 = (vector){xi.first - xj15.first, xi.second - xj15.second};
-          diff16 = (vector){xi.first - xj16.first, xi.second - xj16.second};
           r1  = diff1.first  * diff1.first  + diff1.second  * diff1.second;
           r2  = diff2.first  * diff2.first  + diff2.second  * diff2.second;
           r3  = diff3.first  * diff3.first  + diff3.second  * diff3.second;
           r4  = diff4.first  * diff4.first  + diff4.second  * diff4.second;
-          r5  = diff5.first  * diff5.first  + diff5.second  * diff5.second;
-          r6  = diff6.first  * diff6.first  + diff6.second  * diff6.second;
-          r7  = diff7.first  * diff7.first  + diff7.second  * diff7.second;
-          r8  = diff8.first  * diff8.first  + diff8.second  * diff8.second;
-          r9  = diff9.first  * diff9.first  + diff9.second  * diff9.second;
-          r10 = diff10.first * diff10.first + diff10.second * diff10.second;
-          r11 = diff11.first * diff11.first + diff11.second * diff11.second;
-          r12 = diff12.first * diff12.first + diff12.second * diff12.second;
-          r13 = diff13.first * diff13.first + diff13.second * diff13.second;
-          r14 = diff14.first * diff14.first + diff14.second * diff14.second;
-          r15 = diff15.first * diff15.first + diff15.second * diff15.second;
-          r16 = diff16.first * diff16.first + diff16.second * diff16.second;
           r1  = sqrt(r1);
           r2  = sqrt(r2);
           r3  = sqrt(r3);
           r4  = sqrt(r4);
-          r5  = sqrt(r5);
-          r6  = sqrt(r6);
-          r7  = sqrt(r7);
-          r8  = sqrt(r8);
-          r9  = sqrt(r9);
-          r10 = sqrt(r10);
-          r11 = sqrt(r11);
-          r12 = sqrt(r12);
-          r13 = sqrt(r13);
-          r14 = sqrt(r14);
-          r15 = sqrt(r15);
-          r16 = sqrt(r16);
           if(r1  < Hradius) KernelAndGradient_unidirectional(diff1, i, j    , r1);
           if(r2  < Hradius) KernelAndGradient_unidirectional(diff2, i, j + 1, r2);
           if(r3  < Hradius) KernelAndGradient_unidirectional(diff3, i, j + 2, r3);
           if(r4  < Hradius) KernelAndGradient_unidirectional(diff4, i, j + 3, r4);
-          if(r5  < Hradius) KernelAndGradient_unidirectional(diff5, i, j + 4, r5);
-          if(r6  < Hradius) KernelAndGradient_unidirectional(diff6, i, j + 5, r6);
-          if(r7  < Hradius) KernelAndGradient_unidirectional(diff7, i, j + 6, r7);
-          if(r8  < Hradius) KernelAndGradient_unidirectional(diff8, i, j + 7, r8);
-          if(r9  < Hradius) KernelAndGradient_unidirectional(diff9, i, j + 8, r9);
-          if(r10 < Hradius) KernelAndGradient_unidirectional(diff10, i, j + 9, r10);
-          if(r11 < Hradius) KernelAndGradient_unidirectional(diff11, i, j + 10, r11);
-          if(r12 < Hradius) KernelAndGradient_unidirectional(diff12, i, j + 11, r12);
-          if(r13 < Hradius) KernelAndGradient_unidirectional(diff13, i, j + 12, r13);
-          if(r14 < Hradius) KernelAndGradient_unidirectional(diff14, i, j + 13, r14);
-          if(r15 < Hradius) KernelAndGradient_unidirectional(diff15, i, j + 14, r15);
-          if(r16 < Hradius) KernelAndGradient_unidirectional(diff16, i, j + 15, r16);
         }
 
       }
